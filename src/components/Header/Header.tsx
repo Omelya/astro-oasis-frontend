d0771d5ca458beff5966200b7ffd70a001127631
Vladyslav Omelianenko <vladyslav.omelianenko@meest.com>
2024-07-07 14:41:20 +0300

    Create

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,9 +9,11 @@ import {Button} from "@/ui/Buttons/Button";
 import Logo from '../../../public/static/png/logo.png';
 import {useI18n} from "@/utils/hooks/useI18n";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export const Header = () => {
     const locale = useI18n();
+    const path = usePathname();
 
     return (
         <Stack width={'100%'} direction={'row'} justifyContent={'space-between'}>
@@ -21,9 +23,15 @@ export const Header = () => {
                     <Typography color={'#FFFFFF'} fontSize={37}>ASTRO OASIS</Typography>
                 </Stack>
             </Link>
-            <Button href={'/login'}>
+            <Button href={ path === '/login' ? '/signup' : '/login' }>
                 <Typography color={'#FFFFFF'} fontSize={30}>
-                    { locale?.header.login }
+
+                    {
+                        path === '/login'
+                            ? locale?.welcome.registration
+                            : locale?.header.login
+                    }
+
                 </Typography>
             </Button>
         </Stack>
